d52ba22ee2baa9e8eaa71ae01bdb4f83b3c742b8
Shadow32055 <shadow32055@gmail.com>
2024-01-16 13:20:28 -0500

    Fix localization

--- BetterFocusPoints/Localizations/RefValues.cs
+++ BetterFocusPoints/Localizations/RefValues.cs
@@ -1,11 +1,11 @@
 ﻿namespace BetterFocusPoints.Localizations {
     public class RefValues {
-        public const string FocusText = "{BA_OUkZom}Focus Points";
+        public const string FocusText = "{BFP_93TThc}Focus Points";
 
-        public const string FocusPerLevelText = "{=BA_S7nfeK}Focus Points Per Level";
-        public const string FocusPerLevelHint = "{=BA_GtIuji}How many focus points per level.";
+        public const string FocusPerLevelText = "{=BFP_S7nfeK}Focus Points Per Level";
+        public const string FocusPerLevelHint = "{=BFP_GtIuji}How many focus points per level.";
 
-        public const string MaxFocusText = "{=BA_S5j8sK}Max Focus Points Per Skill";
-        public const string MaxFocusHint = "{=BA_GtIutr}How many focus points that can be spent on a skill.";
+        public const string MaxFocusText = "{=BFP_S5j8sK}Max Focus Points Per Skill";
+        public const string MaxFocusHint = "{=BFP_GtIutr}How many focus points that can be spent on a skill.";
     }
 }
